ffa1543af95422e6f5de870f61b44e3b53b3f317
liamdotpro <liam@liam.pro>
2023-05-19 05:16:36 -0400

    :wrench: fix punts

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -104,7 +104,7 @@ export default function Example() {
                     <div className="mx-auto max-w-2xl text-center">
                         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Get in touch</h2>
                         <p className="mt-2 text-lg leading-8 text-gray-600">
-                            Let's talk about how we can work together to create something great.
+                            Let&apos;s talk about how we can work together to create something great.
                         </p>
                     </div>
                     <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mt-16 max-w-xl sm:mt-20">
@@ -275,7 +275,7 @@ export default function Example() {
                                 type="submit"
                                 className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                                Let's talk
+                                Let&apos;s talk
                             </button>
                         </div>
                     </form>
